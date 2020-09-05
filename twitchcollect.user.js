54dd059745491551116642545d4da00f9d6c69cf
Maximilian <Shawak@users.noreply.github.com>
2020-09-05 19:15:22 +0200

    Create twitchcollect.user.js

---
+++ twitchcollect.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         TwitchCollect
+// @namespace    https://github.com/Shawak/TwitchCollect
+// @author       Shawak
+// @version      0.0.1
+// @description  Automatically collect twitch channel points
+// @match        *://*.twitch.tv/*
+// @downloadURL  https://github.com/Shawak/TwitchCollect/raw/master/twitchcollect.user.js
+// @updateURL    https://github.com/Shawak/TwitchCollect/raw/master/twitchcollect.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function collect() {
+        const elements = document.getElementsByClass('claimable-bonus__icon');
+        for (let element of elements) {
+            element.click();
+        }
+    }
+
+    setTimeout(collect, 5000);
+
+})();
